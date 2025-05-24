c57bf003e02ba2853606b2e2519de454398025bf
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-05-24 14:42:40 +0530

    used redis for rate limiting(upstash) and completed backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRouter from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 const PORT = process.env.PORT || 5001;
 
@@ -10,9 +11,16 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(rateLimiter);
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
+//   next();
+// });
 app.use("/api/notes", notesRouter);
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server started at PORT:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started at PORT:${PORT}`);
+  });
 });
