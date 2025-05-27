eb3e267997726bfb3d86465bdee1336f5a0bc57a
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-05-27 22:11:09 +0530

    changes for deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully!!");
+    //console.log("MongoDB connected successfully!!");
   } catch (error) {
-    console.error("Error connecting with MongoDB", error);
+    //console.error("Error connecting with MongoDB", error);
     process.exit(1); // exit with faiure
   }
 };
